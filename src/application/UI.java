d511083eb7d4751a150da5e531c9793cad385abd
My-Name-Is-Rafael-Sampaio <rafaelsampaiodejesus.30@gmail.com>
2022-12-27 15:20:06 -0300

    handling exceptions and clearing screen

--- src/application/UI.java
+++ src/application/UI.java
@@ -54,6 +54,12 @@ public class UI {
 		System.out.println("  a b c d e f g h   <-- Coluna");
 	}
 
+	// https://stackoverflow.com/questions/2979383/java-clear-the-console
+	public static void clearScreen() {
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}
+
 	public static ChessPosition readChessPosition(Scanner sc) {
 		try {
 			String coordinate = sc.nextLine();
