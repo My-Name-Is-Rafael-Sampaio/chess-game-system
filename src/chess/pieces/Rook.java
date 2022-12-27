b8a170c12460406f21c00bb81424d79a91cd5740
My-Name-Is-Rafael-Sampaio <rafaelsampaiodejesus.30@gmail.com>
2022-12-27 16:23:09 -0300

    implementing possible rook moves with a small improvement

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardGame.Board;
+import boardGame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -9,14 +10,55 @@ public class Rook extends ChessPiece {
 		super(board, color);
 	}
 
-	@Override
-	public String toString() {
-		return "R";
+	private void opponentHere(Position position, boolean[][] tempMatrix) {
+		if (getBoard().positionExists(position) && isThereOpponentPiece(position)) {
+			tempMatrix[position.getRow()][position.getColumn()] = true;
+		}
 	}
 
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] temporary = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+		Position auxiliaryPosition = new Position(0, 0);
+
+		// above
+		auxiliaryPosition.setValues(position.getRow() - 1, position.getColumn());
+		while (getBoard().positionExists(auxiliaryPosition) && !getBoard().thereIsAPiece(auxiliaryPosition)) {
+			temporary[auxiliaryPosition.getRow()][auxiliaryPosition.getColumn()] = true;
+			auxiliaryPosition.setRow(auxiliaryPosition.getRow() - 1);
+		}
+		opponentHere(auxiliaryPosition, temporary);
+
+		// left
+		auxiliaryPosition.setValues(position.getRow(), position.getColumn() - 1);
+		while (getBoard().positionExists(auxiliaryPosition) && !getBoard().thereIsAPiece(auxiliaryPosition)) {
+			temporary[auxiliaryPosition.getRow()][auxiliaryPosition.getColumn()] = true;
+			auxiliaryPosition.setColumn(auxiliaryPosition.getColumn() - 1);
+		}
+		opponentHere(auxiliaryPosition, temporary);
+
+		// right
+		auxiliaryPosition.setValues(position.getRow(), position.getColumn() + 1);
+		while (getBoard().positionExists(auxiliaryPosition) && !getBoard().thereIsAPiece(auxiliaryPosition)) {
+			temporary[auxiliaryPosition.getRow()][auxiliaryPosition.getColumn()] = true;
+			auxiliaryPosition.setColumn(auxiliaryPosition.getColumn() + 1);
+		}
+		opponentHere(auxiliaryPosition, temporary);
+
+		// below
+		auxiliaryPosition.setValues(position.getRow() + 1, position.getColumn());
+		while (getBoard().positionExists(auxiliaryPosition) && !getBoard().thereIsAPiece(auxiliaryPosition)) {
+			temporary[auxiliaryPosition.getRow()][auxiliaryPosition.getColumn()] = true;
+			auxiliaryPosition.setRow(auxiliaryPosition.getRow() + 1);
+		}
+		opponentHere(auxiliaryPosition, temporary);
+
 		return temporary;
+	}
+
+	@Override
+	public String toString() {
+		return "R";
 	}
 }
