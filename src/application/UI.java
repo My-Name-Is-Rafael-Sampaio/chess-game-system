3d546617878152321087fc2fb5a4c43f53de1a62
My-Name-Is-Rafael-Sampaio <rafaelsampaiodejesus.30@gmail.com>
2022-12-27 12:05:03 -0300

    initial implementation of the method of moving and capturing the pieces

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,6 +1,10 @@
 package application;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 import chess.ChessPiece;
+import chess.ChessPosition;
 import chess.Color;
 
 public class UI {
@@ -48,5 +52,16 @@ public class UI {
 		}
 		System.out.println();
 		System.out.println("  a b c d e f g h   <-- Coluna");
+	}
+
+	public static ChessPosition readChessPosition(Scanner sc) {
+		try {
+			String coordinate = sc.nextLine();
+			char column = coordinate.charAt(0);
+			int row = Integer.parseInt(coordinate.substring(1));
+			return new ChessPosition(column, row);
+		} catch (RuntimeException e) {
+			throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8.");
+		}
 	}
 }
--- src/boardGame/Board.java
+++ src/boardGame/Board.java
@@ -58,4 +58,17 @@ public class Board {
 		this.pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
 	}
+
+	public Piece removePiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		if (piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		this.pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+	}
 }
